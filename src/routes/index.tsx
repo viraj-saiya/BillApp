1383c27d63c226233fad87ccb352988c1bb2c164
viraj-saiya <saiyaviraj04@gmail.com>
2025-08-19 22:52:07 +0530

    Add landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
-import App from '../App'
+import { createFileRoute } from '@tanstack/react-router';
+import LandingPage from '../feature/LandingPage/LandingPage';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -7,9 +7,9 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-      <App/>
+    <div >
+      <LandingPage/>
+      {/* <App/> */}
     </div>
   )
 }
